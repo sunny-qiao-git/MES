1745d9a116bbc9ce0b390867e924eba3549e3896
jion137 <jion137@hotmail.com>
2020-05-06 09:37:42 +0800

    增加操作提示的菜单

---
+++ Web/AppMes/Demo/HelpTip.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="HelpTip.aspx.cs" Inherits="AppMes_Demo_HelpTip" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="root">
+            <asp:ETitle runat="server" ID="pagetitle" Title="导出离线资源"></asp:ETitle>
+
+            <div id="toolbar" class="toolbar">
+              <asp:EButton runat="server" ID="btnSave" Text="导出离线资源"  OnClick="btnExport_Click" Icon="save" />
+
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Web/AppMes/HelpTip/frmHelpTepList.aspx.cs
@@ -0,0 +1,52 @@
+﻿using ECI.MES.ServiceList;
+using PL.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AppMes_HelpTip_frmHelpTepList : QueryPage
+{
+    private string catelogid
+    {
+        get { return this.hidCatalogId.Value; }
+        set { this.hidCatalogId.Value = value; }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            this.catelogid = PageHelper.Request("fguid");
+
+            if (this.catelogid.HasValue())
+            {
+                this.Search();
+            }
+        }
+    }
+    public override void QuerySetting(EciRequest request)
+    {
+        request.ServiceId = MESService.MesBdZyStatusSearch;
+
+        request.Entity = new EntityBase();
+        request.Entity.GetBindingData();
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        Button btn = sender as Button;
+        string key = btn.CommandArgument;
+
+        EciRequest request = new EciRequest(MESService.MesBdZyStatusDelete);
+
+        request.ListKey = new List<string>() { key };
+
+        SOA.Execute(request);
+
+        EAlert.Tip("删除执行成功");
+
+        this.Search();
+    }
+}
